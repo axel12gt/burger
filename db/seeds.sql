e28cf4fe4a9d3ee4ee22ecc34cf477cd60ae1fd0
Dameon Brown <axel12gt@live.com>
2019-07-10 09:26:35 -0400

    Finished the db and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Bacon");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Ham and Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Ham and Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Plain");
